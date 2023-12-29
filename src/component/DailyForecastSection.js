b79a34077751069437cffc2b4c94de32e837e008
Tisha Saha <tishasaha32@gmail.com>
2023-12-29 18:41:39 +0530

    daily Forecast added

--- src/component/DailyForecastSection.js
+++ src/component/DailyForecastSection.js
@@ -2,18 +2,32 @@ import React, { useEffect } from "react";
 import useDailyForecast from "../hooks/useDailyForecast";
 import useLocation from "../hooks/useLocation";
 
+import styles from "./DailyForecastSection.module.css";
+
 function DailyForecastSection() {
   const { location } = useLocation();
-  const { getDailyForecast } = useDailyForecast(
+  const { dailyForecast, getDailyForecast } = useDailyForecast(
     location?.latitude,
     location?.longitude
   );
-
   useEffect(() => {
     getDailyForecast();
   }, [location]);
 
-  return <div>DailyForecastSection</div>;
+  return (
+    <div className={styles.dailyForecastContainer}>
+      <p style={{ fontWeight: "bolder" }}>Next 8 Days Forecast</p>
+
+      {dailyForecast?.map((daily, index) => {
+        return (
+          <div key={index}>
+            <p>{daily.temp}°C</p>
+            <p>{daily.day}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default DailyForecastSection;
--- src/hooks/useDailyForecast.js
+++ src/hooks/useDailyForecast.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function useDailyForecast(latitude, longitude) {
+  const [dailyForecast, setDailyForecast] = useState([
+    {
+      day: null,
+      temp: null,
+    },
+  ]);
+
   const getDailyForecast = () => {
     const apiKey = "a725ada07003dd768b8f3baa88c13290";
     const apiUrlDaily = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely&appid=${apiKey}&units=metric`;
-  };
-  fetch(apiUrlDaily)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
 
-  return { getDailyForecast };
+    fetch(apiUrlDaily)
+      .then((response) => response.json())
+      .then((data) => {
+        let temperature = 0;
+        const dailyWeather = data.daily;
+        const updatedDailyForecast = dailyWeather.map((daily) => {
+          temperature = Math.floor((daily.temp.max + daily.temp.min) / 2);
+          const milliseconds = daily.dt * 1000;
+          const dateObject = new Date(milliseconds);
+          const dayOfWeek = dateObject.toLocaleDateString("en-US", {
+            weekday: "long",
+          });
+
+          return { day: dayOfWeek, temp: temperature };
+        });
+        updatedDailyForecast[0].day = "Tomorrow";
+        setDailyForecast(updatedDailyForecast);
+      })
+      .catch((error) => {
+        console.error("Error fetching daily forecast:", error);
+      });
+  };
+  return { dailyForecast, getDailyForecast };
 }
 
 export default useDailyForecast;
